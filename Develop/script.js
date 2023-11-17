cc181a0fb15a355131fbd64c7a30ce6806c8a06f
victorgarrido1 <vcgarr94@gmail.com>
2023-11-16 17:33:42 -0700

    First commit

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,27 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var timeBox = $(".time-block");
+var textarea = $(".textarea");
+var saveButton = $(".saveBtn");
+var hourBox = $("button");
+var headline = $("#currentDay")
+var currentTime = dayjs().format('MMM DD, YYYY h:s a');
+var value = $(this)
+
+
+
+
+
+console.log(currentTime);
+
+
+
 $(function () {
+  currentHour = dayjs(hourBox.val())
+saveButton.on('click', function () {
+  $('.saveBtn').
+})
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
